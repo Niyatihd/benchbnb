b622898919c82601d57ea5dc2694329c16f6bb95
Niyati Desai <niyatihd@gmail.com>
2019-05-19 16:14:38 -0700

    Create application controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,19 @@
 class Api::UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login(@user)
+      render 'api/user/show'
+    else
+      render json: ["Invalid Username/Password"], status: 420
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password, :email)
+  end
 end
